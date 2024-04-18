8052e34d99a70088ee38409df395230fa8439f17
Abdulaziz Chalya <abdulazizchalya@gmail.com>
2024-04-18 19:21:22 -0400

    image bug fix

--- images/images.js
+++ images/images.js
@@ -1,2 +1,13 @@
 export const homePageDrMaddox = require('../assets/homePageDrMaddox.png');
 export const bgImage = require('../assets/bg.png');
+
+export const agenda_active = require('../assets/Active/agenda_active.svg');
+export const agenda_inactive = require('../assets/Inactive/agenda_inactive.svg');
+export const map_active = require('../assets/Active/map_active.svg');
+export const map_inactive = require('../assets/Inactive/map_inactive.svg');
+export const location_active = require('../assets/Active/location_active.svg');
+export const location_inactive = require('../assets/Inactive/location_inactive.svg');
+export const FAQ_active = require('../assets/Active/FAQ_active.svg');
+export const FAQ_inactive = require('../assets/Inactive/FAQ_inactive.svg');
+
+export const settings_inactive = require('../assets/settings_inactive.svg');
